39499e5fa2b91247c28c271d1cefd4e52bb021ae
sasha <sasha.ts91@gmail.com>
2018-08-31 22:20:31 +0300

    file uploading/downloading and writing done

--- app.py
+++ app.py
@@ -4,8 +4,7 @@ import lz77_rust
 app = Flask(__name__)
 
 
-def get_template_params(text: str):
-    (compressed, decompressed), (size_before, size_after) = lz77_rust.get_results(text)
+def get_template_params(text, compressed, decompressed, size_before, size_after):
     return {
         'initial_text': text,
         'compressed_text': compressed,
@@ -15,16 +14,31 @@ def get_template_params(text: str):
     }
 
 
+def write_to_file(decompressed_text, output_path):
+    with open(output_path, 'w', encoding='utf-8-sig') as file:
+        file.write(decompressed_text)
+
+
 @app.route('/', methods=['GET', 'POST'])
 def process_compress_request():
     if request.method == 'GET':
         return render_template('index.html')
     elif request.method == 'POST':
         params = request.form
-        print(params)
-        if 'text_to_compress' in params:
+        files = request.files
+        if 'file' in files:
+            file = files['file']
+            text = file.read().decode('utf-8-sig')
+            (compressed, decompressed), (size_before, size_after) = lz77_rust.get_results(text)
+            write_to_file(decompressed, 'output.txt')
             return render_template('index.html',
-                                   **get_template_params(params['text_to_compress']))
+                                   **get_template_params(text, compressed, decompressed, size_before, size_after))
+        elif 'text_to_compress' in params:
+            text = params['text_to_compress']
+            (compressed, decompressed), (size_before, size_after) = lz77_rust.get_results(text)
+            write_to_file(decompressed, 'output.txt')
+            return render_template('index.html',
+                                   **get_template_params(text, compressed, decompressed, size_before, size_after))
         elif 'compression_algorithm' in params:
             return render_template('index.html',
                                    decompressed_text=params['compression_algorithm'],)
